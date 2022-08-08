d7b2e37867bd0bbe78edd53db32a06d08dc3b30d
DESKTOP-O8GGFOL\user <dlacoaud92@naver.com>
2022-08-08 22:14:32 +0900

    2022-08-08 각각의 도메인 엔티티 클래스 설정 h2 데이터베이스에 입력완료

---
+++ jpabook/src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// 롬복 설정 됬는지 확인
+@Getter
+@Setter
+public class Hello {
+ private String data;
+}
---
+++ jpabook/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter @Setter
+public class Member {
+   @Id  @GeneratedValue
+   @Column(name = "member_id")
+	private Long id;
+   
+   private String name;
+   
+   @Embedded
+   private Address address;
+   
+   @JsonIgnore
+   @OneToMany(mappedBy = "member") // order테이블에 있는 member에의해 맵핑된거 뿐
+   private List<Order> orders = new ArrayList<>();
+}
